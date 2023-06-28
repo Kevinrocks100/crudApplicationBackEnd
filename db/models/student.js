1bd62548b1007ec546b468cd7444bb7037fdb49a
Kevin Liu <you@example.com>
2023-06-28 19:26:12 -0400

    Created a student model

---
+++ db/models/student.js
@@ -0,0 +1,40 @@
+const { DataTypes } = require('sequelize');
+const db = require('../db');
+
+const Student = db.define('student', {
+  firstName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  lastName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+      isEmail: true,
+    },
+  },
+  imageUrl: {
+    type: DataTypes.STRING,
+    defaultValue: 'default_image_url.jpg', 
+  },
+  gpa: {
+    type: DataTypes.DECIMAL(1, 1),
+    validate: {
+      min: 0.0,
+      max: 4.0,
+    },
+  },
+});
+
+module.exports = Student;
