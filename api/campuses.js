ded6728106d0f84bfad2b9bc9fea6eff45089afe
Kevin Liu <you@example.com>
2023-07-04 13:04:44 -0400

    a route to remove a campus (based on its id)

--- api/campuses.js
+++ api/campuses.js
@@ -41,4 +41,15 @@ router.post("/add", async (req, res, next) => {
   }
 });
 
+router.delete("/delete/:id", async (req, res, next) => {
+  try {
+    const RemoveCampus = await Campus.destroy({ where: { id: req.params.id } });
+    RemoveCampus
+      ? res.status(200).send("Campus removed successfully")
+      : res.status(404).send("Campus Not Found");
+  } catch (error) {
+    next(error);
+  }
+});
+
 module.exports = router; 
